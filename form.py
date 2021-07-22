471d8fc7020d7fba66a333ccb1801e3d0f0df90a
Wenny <wennycheng21@gmail.com>
2021-07-22 18:18:55 +0000

    2:18

--- form.py
+++ form.py
@@ -1,15 +1,21 @@
-import random 
+import random
 
 print("Welcome to Homeless Resources! Please fill the form below to find a nearby shelters and inns.")
 
+# this code asks for user inputs of the questions the forms asks them
 state_id = ""
 user_state = input("What state do you live in? ")
-user_age = input("What is your current age? ")
+user_age = int(input("What is your current age? "))
 user_gender = input("What is your gender? (Female, Male, or N/A) ")
 user_assistance = input(
     "What is the assistance you will need? (Food, Shelter, Hygiene, Job Assistance, Transportation) ")
+
+user_agerange = ""
+# this code generates a random 4 digit number in the range of 1000 and 9999
 id_number = random.randrange(1000, 9999)
 
+
+# this code is a dictionary that creates the first two letters of their assigned ID using their state when they fill the form
 state = {
     "new york": "NY",
     "alabama": "AL",
@@ -63,6 +69,17 @@ state = {
     "wyoming": "WY"
 }
 
+# creating age ranges based on the user's age
+if(0 <= user_age <= 14):
+    user_agerange = "children"
+elif(15 <= user_age <= 24):
+    user_agerange = "youth"
+elif(25 <= user_age <= 64):
+    user_agerange = "adult"
+elif (user_age >= 65):
+    user_agerange = "senior"
 
-print("Thank you for filling the form! Here's the info we received: State: " + user_state + ", Age: " + str(user_age) +
-      ", Gender: " + user_gender + ", Assistance: " + user_assistance + ". Your assigned ID will be " + state[user_state.lower()] + str(id_number)) 
+
+# output after recieving the user's info in the form
+print("Thank you for filling the form! Here's the info we received: State: " + user_state + ", Age Range: " + user_agerange +
+      ", Gender: " + user_gender + ", Assistance: " + user_assistance + ". Your assigned ID will be " + state[user_state.lower()] + str(id_number))
