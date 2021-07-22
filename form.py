0076e0de291809e32b35ef338e01f77c1aaeb3aa
Wenny <wennycheng21@gmail.com>
2021-07-22 16:02:15 +0000

    form

---
+++ form.py
@@ -0,0 +1,5 @@
+print("Welcome to Homeless Resources! Please fill the form below to find a nearby shelters and inns.")
+
+user_state = input("What state do you live in?")
+user_age = input("What is your current age?")
+user_gender = input("What is your gender? Female, Male, or N/A")
