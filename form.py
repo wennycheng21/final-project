d5e37e52e4f3d921e99ac47579f1920f67817f60
Wenny <wennycheng21@gmail.com>
2021-07-22 16:11:37 +0000

    new update 12:11

--- form.py
+++ form.py
@@ -1,5 +1,8 @@
 print("Welcome to Homeless Resources! Please fill the form below to find a nearby shelters and inns.")
 
-user_state = input("What state do you live in?")
-user_age = input("What is your current age?")
-user_gender = input("What is your gender? Female, Male, or N/A")
+user_state = input("What state do you live in? ")
+user_age = input("What is your current age? ")
+user_gender = input("What is your gender? Female, Male, or N/A ")
+user_assistance = input("What is the first assistance you will need? Food, Shelter, Hygiene, Job assistance, Transportation ")
+
+print("Thank you for filling the form! Your state: " + user_state +  ", Age: " + str(user_age) + ", Gender: " + user_gender + ".")
