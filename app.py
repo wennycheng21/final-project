c30dfb7221146403b63820c9770b4d68db539683
Wenny <wennycheng21@gmail.com>
2021-07-26 16:08:31 +0000

    12:08

--- app.py
+++ app.py
@@ -7,16 +7,12 @@ from flask_pymongo import PyMongo
 from flask import redirect
 from flask import session
 import os
+from model import getShelter
+from datetime import datetime
 
 
 # -- Initialization section --
 app = Flask(__name__)
-
-# events = [
-#         {"event":"First Day of Classes", "date":"2019-08-21"},
-#         {"event":"Winter Break", "date":"2019-12-20"},
-#         {"event":"Finals Begin", "date":"2019-12-01"}
-#     ]
 
 # name of database
 app.secret_key = os.getenv("SECRET_KEY")
@@ -25,8 +21,7 @@ uri_password = os.getenv("PASSWORD")
 app.config['MONGO_DBNAME'] = 'database'
 
 # URI of database
-app.config['MONGO_URI'] = 'mongodb+srv://admin:' + uri_password + \
-    '@cluster0.lndrp.mongodb.net/database?retryWrites=true&w=majority'
+app.config['MONGO_URI'] = 'mongodb+srv://admin:' + uri_password + '@cluster0.lndrp.mongodb.net/database?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
@@ -67,36 +62,44 @@ def yourShelter():
 
 # login page
 
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
     if request.method == "GET":
-        return render_template("account.html")
+        return render_template("signup.html")
     else:
         # this creates a user's database in mongo db if it doesn't already exist
-
+ 
         users = mongo.db.users
-
+ 
         # this stores form data into a user's dictionary
         user = {
             "username": request.form["username"],
             "password": request.form["password"]
         }
-
+ 
         # checks if user already exists in the database
         existing_user = users.find_one({'username': user['username']})
+        
+        
         # make condition to check if user already exists in mongo
         if existing_user is None:
             users.insert(user)  # add our user data into mongo
+      
+      
        # tell the browser session who the user is
             session["username"] = request.form["username"]
-            #return "Congratulations, you made an account: @ " + request.form["username"]
+            
+            
+            # return "Congratulations, you made an account: @ " + request.form["username"]
             return render_template("index.html")
         else:
             return "Unfortunately, the username is taken."
+ 
+ 
+
 
 @app.route('/logout')
 def logout():
-#removes session
+    # removes session
     session.clear()
     return redirect('/')
